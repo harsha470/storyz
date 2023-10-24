1f418fc716da981d66ccd11823e1271cb4c342d6
madasi vardhan <madasi.vardhan@ril.com>
2023-10-24 17:04:54 +0530

    add gradient color

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,15 @@
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <h1 className="mb-5 text-5xl font-bold">Welcome to StoryZ</h1>
+      <h1 className="mb-5 text-5xl font-bold">
+        Welcome to{" "}
+        <span className="text-transparent bg-gradient bg-clip-text">
+          StoryZ
+        </span>
+      </h1>
       <p>
-        StoryZ is a simple blog application that allows users to
-        effortlessly share their stories and connect with their audience.
+        StoryZ is a simple blog application that allows users to effortlessly
+        share their stories and connect with their audience.
       </p>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient: "linear-gradient(90deg, #ab87ff, #3ce9a4)",
+      },
+    },
   },
   plugins: [],
-}
+};
