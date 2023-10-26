1d724db8c61c1e8e46f19596d4fc655b56857f2e
madasi vardhan <madasi.vardhan@ril.com>
2023-10-26 16:56:33 +0530

    make app name an env variable

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,17 @@
+const APP_NAME = import.meta.env.VITE_APP_NAME;
+
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <h1 className="mb-5 text-5xl font-bold">
         Welcome to{" "}
         <span className="text-transparent bg-gradient-to-r from-[#ab87ff] to-[#3ce9a4] bg-clip-text">
-          StoryZ
+          {APP_NAME}
         </span>
       </h1>
       <p>
-        StoryZ is a simple blog application that allows users to effortlessly
-        share their stories and connect with their audience.
+        {APP_NAME} is a simple blog application that allows users to
+        effortlessly share their stories and connect with their audience.
       </p>
     </div>
   );
